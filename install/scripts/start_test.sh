a8fff3c947f01c5a2c369e348ba7b0ce4603d583
r2h2 <rainer@hoerbe.at>
2016-08-22 11:51:15 +0200

    add test config

--- install/scripts/start_test.sh
+++ install/scripts/start_test.sh
@@ -3,12 +3,14 @@
 echo "starting nginx in daemon mode"
 /usr/local/nginx/sbin/nginx -c /opt/nginx_test/etc/nginx/nginx.conf
 
+cd /opt/nginx_test/var/log/
+
 echo "GET http://localhost:8080/index.html?xyz=testnaxsitest"
 curl http://localhost:8080/index.html?xyz=testnaxsitest
 
 echo "GET http://localhost:8080/index.html?abc=--&def=--"
 curl http://localhost:8080/index.html?abc=--&def=--
 
-echo "The requests should have triggered NAXSI messages in the error log (/opt/nginx_test/var/log/error.log)"
+echo "The requests should have triggered HTTP code 418 responses and NAXSI messages in the error log (/opt/nginx_test/var/log/error.log)"
 
 bash
